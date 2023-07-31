412d8ea30754d926152b847c9732d2f9b09a2a46
bokuanT <e0701815@u.nus.edu>
2023-08-01 01:39:35 +0800

    Make compatible with ngrok

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_ngrok import run_with_ngrok
+from web_scraping.scrape import get_webscrape_data
 
+CACHED_DATA = get_webscrape_data()
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/get', methods = ['GET'])
 def get_articles():
-    return jsonify({"Hello":"world"})
+    return jsonify(CACHED_DATA)
 
-
+@app.route('/', methods = ['GET'])
+def default_test():
+    return "Hello world"
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ web_scraping/scrape.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+def get_webscrape_data():
+    ## selenium used to scroll to the bottom of the page
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+
+    driver.get("https://shop.chope.co/collections/best-sellers")
+
+    SCROLL_PAUSE_TIME = 3
+
+    # Get the height of the page
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll to the bottom of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new height and check if we have reached the end of the page
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        
+        last_height = new_height
+
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    # getting the relevant data
+    all_deals = soup.find(id = 'bc-sf-filter-products')
+    url = 'https://shop.chope.co'
+    promos = []
+
+    def strip_things(string):
+        output = string.replace("<p>",'')
+        output = output.replace("</p>",'')
+        output = output.replace("<br/>",", ")
+        return output
+
+    for deal in all_deals:
+        
+        title = deal.select('a.color-blue.app-link') # restaurant / name of deal
+        links = deal.select('a')
+        link = links[0].get('href') # get url to go to the deal page
+        
+        deal_link = url+link
+        deal_page = requests.get(deal_link)
+        deal_soup = BeautifulSoup(deal_page.content, 'html.parser')
+        infos = deal_soup.select('div.product-desc')
+        info = infos[0].select('strong')[0].text #brief info on deal
+        
+        cards = deal_soup.select('div.details')
+        for card in cards:
+
+            header = card.select('h5.header-xs.color-blue')
+            
+            if header == []:
+                continue
+            
+            if header[0].text == "Address":
+                address = str(card.select('p')[0])
+                address = strip_things(address)  # address of restaurant
+        
+        promos.append({
+            "title": title[0].text,
+            "info": info,
+            "address": address,
+
+        })
+    return promos
+
