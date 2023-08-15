2a0eedb36f124b4f79924d5960fe931639fb1fc5
bokuanT <e0701815@u.nus.edu>
2023-08-15 20:10:36 +0800

    Add more info to deal detail page

--- web_scraping/scrape.py
+++ web_scraping/scrape.py
@@ -14,7 +14,7 @@ def get_webscrape_data():
 
     driver.get("https://shop.chope.co/collections/best-sellers")
 
-    SCROLL_PAUSE_TIME = 2
+    SCROLL_PAUSE_TIME = 0.5
 
     # Get the height of the page
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -45,7 +45,7 @@ def get_webscrape_data():
         output = output.replace("</p>",'')
         output = output.replace("<br/>",", ")
         return output
-    i = 0
+
     for deal in all_deals:
         title = deal.select('a.color-blue.app-link') # restaurant / name of deal
         links = deal.select('a')
@@ -57,12 +57,38 @@ def get_webscrape_data():
         infos = deal_soup.select('div.product-desc')
         info = infos[0].select('strong')[0].text #brief info on deal
         
+        # Get voucher details
+        vouchers_div = deal_soup.select('div.product-variants.hide-mobile li.child.relative')#deal_soup.select('div.product-variants.hide-mobile')
+        cur_deal_vouchers = []
+        for voucher in vouchers_div:
+            # Extracting date
+            date = voucher.select_one('div.date.color-blue.body-s.bold-700')
+            if date:
+                date = date.text.strip()
+
+            # Extracting time
+            time_ = voucher.select_one('div.time.body-s.color-darkgrey.mb-5')
+            if time_:
+                time_ = time_.text.strip()
+
+            # Extracting price (both discounted and original)
+            price_discounted = voucher.select_one('div.product-price strong.price.color-orange')
+            if price_discounted:
+                price_discounted = price_discounted.text.strip()
+            
+            price_original = voucher.select_one('div.product-price strike.color-darkgrey.body-s')
+            if price_original:
+                price_original = price_original.text.strip()
+
+            product_savings = voucher.select_one('div.child-right div.product-savings.big')
+            if product_savings:
+                product_savings = product_savings.text.strip()
+
+            cur_voucher = {"date": date, "time": time_, "price_discounted": price_discounted, "price_original": price_original, "product_savings": product_savings}
+            cur_deal_vouchers.append(cur_voucher)
+
         cards = deal_soup.select('div.details')
-        # if i == 0:
-        #     print(deal_soup)
-        #     print("cards\n")
-        #     print(cards)
-        #     i += 1
+
         for card in cards:
             header = card.select('h5.header-xs.color-blue')
             
@@ -76,7 +102,7 @@ def get_webscrape_data():
             if header[0].text == "Opening Hours":
                 opening_hours_div = card.select_one('div.rte')
                 if opening_hours_div:
-                    opening_hours = opening_hours_div.text
+                    opening_hours = opening_hours_div.text.replace("\n", "")
             
             if header[0].text == "Cuisine":
                 cuisine_div = card.select_one('div.rte')
@@ -95,7 +121,9 @@ def get_webscrape_data():
             "address": address,
             "image": image_url,
             "opening_hours": opening_hours,
-            "tags": tagsList
+            "tags": tagsList,
+            "vouchers": cur_deal_vouchers,
+            "link": deal_link
         })
     return promos
 
